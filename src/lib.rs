94be3a707bc4a40e7b92e3547c3ba045dddb126d
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-11-10 18:22:36 +0900

    WIP: Test for struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,38 @@ pub mod serializer;
 #[cfg(test)]
 mod tests {
     use crate::serializer::*;
-    use pyo3::{prelude::*, types::PyString};
+    use pyo3::{
+        prelude::*,
+        types::{PyDict, PyString},
+    };
+    use serde::Serialize;
 
     #[test]
-    fn test_as_py_object() {
+    fn serialize_string() {
         Python::with_gil(|py| {
             let obj = as_py_object(py, "test").unwrap();
             assert!(obj.is_instance_of::<PyString>());
+        });
+    }
+
+    #[derive(Serialize)]
+    struct A {
+        a: i32,
+        b: String,
+    }
+
+    #[test]
+    fn serialize_struct() {
+        Python::with_gil(|py| {
+            let obj = as_py_object(
+                py,
+                &A {
+                    a: 32,
+                    b: "test".to_string(),
+                },
+            )
+            .unwrap();
+            assert!(obj.is_instance_of::<PyDict>());
         });
     }
 }
