7e3b5e04029474f90bca2985b193f34bde662799
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-12-01 18:53:57 +0900

    More tests

--- tests/to_json_to_pyobject.rs
+++ tests/to_json_to_pyobject.rs
@@ -11,7 +11,7 @@ fn test(obj: impl Serialize) {
     Python::with_gil(|py| {
         let direct = serde_pyobject::to_pyobject(py, &obj).unwrap();
         let by_json = to_json_to_pyobject(py, obj).unwrap();
-        assert!(direct.eq(by_json).unwrap());
+        assert!(dbg!(direct).eq(dbg!(by_json)).unwrap());
     })
 }
 
@@ -21,4 +21,59 @@ fn primitive() {
     test(-4_i32);
     test(-3.1);
     test(true);
+}
+
+#[test]
+fn option() {
+    test(Some(10_u8));
+    test(None::<u8>);
+}
+
+#[test]
+fn unit() {
+    test(());
+}
+
+#[derive(Serialize)]
+struct UnitStruct;
+
+#[test]
+fn unit_struct() {
+    test(UnitStruct);
+}
+
+#[derive(Serialize)]
+enum UnitVariant {
+    A,
+    B,
+}
+
+#[test]
+fn unit_variant() {
+    test(UnitVariant::A);
+    test(UnitVariant::B);
+}
+
+#[derive(Serialize)]
+enum NewtypeVariant {
+    N(u8),
+}
+
+#[test]
+fn newtype_variant() {
+    test(NewtypeVariant::N(10));
+}
+
+#[derive(Serialize)]
+struct A {
+    a: i32,
+    b: String,
+}
+
+#[test]
+fn struct_() {
+    test(A {
+        a: 10,
+        b: "hello".to_owned(),
+    });
 }
