b1806c25e8ecbc5833d42719d5def1fe00bcb74c
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-10-25 18:10:25 +0900

    Do not cast to PyFunction for supporting abi3 case

--- src/dataclass.rs
+++ src/dataclass.rs
@@ -9,16 +9,10 @@ pub fn dataclass_as_dict<'py>(
     let module = PyModule::import(py, "dataclasses").expect("Failed to import dataclasses");
     let is_dataclass_fn = module
         .getattr("is_dataclass")
-        .expect("Failed to get is_dataclass")
-        .cast_into::<PyFunction>()
-        .expect("Failed to cast to PyFunction");
+        .expect("Failed to get is_dataclass");
 
     if is_dataclass_fn.call1((obj,))?.extract::<bool>()? {
-        let asdict_fn = module
-            .getattr("asdict")
-            .expect("Failed to get asdict")
-            .cast_into::<PyFunction>()
-            .expect("Failed to cast to PyFunction");
+        let asdict_fn = module.getattr("asdict").expect("Failed to get asdict");
         let dict_obj = asdict_fn.call1((obj,))?;
         let dict = dict_obj.cast_into::<PyDict>()?;
         Ok(Some(dict))
